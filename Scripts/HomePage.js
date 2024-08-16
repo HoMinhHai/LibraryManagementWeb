9aa2c24407aa64fd2ad67e904861eec952cb4795
Minh Hai <minhhai031212@gmail.com>
2024-08-16 21:06:35 +0700

    Pagination documents for homepage

--- Scripts/HomePage.js
+++ Scripts/HomePage.js
@@ -1,81 +1,38 @@
-﻿
-let lastPage = 0, firstPage = 0, currentPage = 0;
-
-fetchData("book");
-showNumberPages();
-
-
-function addClickPage() {
-    const lstItemPage = document.querySelectorAll(".pagination-homepage li");
-    lstItemPage.forEach(item => {
-        item.addEventListener("click", event => {
-            if (item.innerHTML == '«' || item.innerHTML == '»')
-                return;
-            currentPage = item.textContent
-            lstItemPage.forEach(i => i.classList.remove("active"));
-            event.target.classList.add("active");
-            changeNumberPage()
-        })
-        
-    });
-}
-const changeNumberPage = () => {
-    alert("press the button ")
-}
-function scrollListPages(symbol) {
-    if (symbol == '»')
-        alert(lastPage)
-    else
-        alert(firstPage)
-}
-
-const liElements = document.querySelectorAll(".list_type_doc li");
+﻿const liElements = document.querySelectorAll(".list_type_doc li");
 liElements.forEach(li => {
     li.addEventListener("click", event => {
         const loaiTaiLieu = li.getAttribute("data-type");
         liElements.forEach(i => i.classList.remove("item_selected"));
         event.target.classList.add("item_selected");
-        fetchData(loaiTaiLieu);
+        showNumberPages(loaiTaiLieu, 1);
     });
 });
+const addEffectClickPage = () => {
+    const lstItemPage = document.querySelectorAll(".pagination-homepage li");
+    lstItemPage.forEach(item => {
+        item.addEventListener("click", event => {
+            if (item.textContent == '«')
+                showPreviousPage()
+            else if (item.textContent == '»')
+                showNextPage()
+            else {
+                if (item.textContent != currentPage) {
+                    currentPage = item.textContent
+                    lstItemPage.forEach(i => i.classList.remove("active"));
+                    event.target.classList.add("active");
+                    changeNumberPage(currentPage, numberItemPerPage)
+                }
 
-   
-
-async function showNumberPages() {
-    const url = `/TaiLieu/GetDocumentByCategory?type=book`;
-    const response = await fetch(url, { method: 'GET' });
-    const data = await response.json()
-
-    const numberItemPerPage = 12, limitedDisplayPage = 2;
-    let totalPages = Math.ceil(data.length / numberItemPerPage)
-
-    const paginationElement = document.querySelector('.pagination-homepage')
-    paginationElement.innerHTML = `<li>&laquo;</li>`
-    paginationElement.innerHTML += `<li class = "active">1</li>`
-    if (totalPages > limitedDisplayPage) {
-        for (let i = 2; i <= limitedDisplayPage; i++) {
-            paginationElement.innerHTML += `<li>${i}</li>`
-        }  
-        lastPage = limitedDisplayPage;
-    }
-    else {
-        for (let i = 2; i <= totalPages; i++) {
-            paginationElement.innerHTML += `<li>${i}</li>`
-        }
-        lastPage = totalPages;
-    }
-    firstPage = 1
-    paginationElement.innerHTML += `<li>&raquo;</li>`
-    addClickPage()
+            }
+        })
+    });
 }
-async function fetchData(loaiTaiLieu) {
-    const params = {
-        method: 'GET',
-    };
-    const url = `/TaiLieu/GetDocumentByCategory?type=${loaiTaiLieu}`;
-    const response = await fetch(url, params);
-    const data = await response.json();
-    const danhSachHTML = data.map(item => `
+const changeNumberPage = (currentPage, pageSize) => {
+    const start = (currentPage - 1) * pageSize
+    const end = start + pageSize
+    const listDocumentToDisplay = allDocuments.slice(start, end)
+
+    const danhSachHTML = listDocumentToDisplay.map(item => `
                 <div onClick ="viewDetail(${item.ID_TL})" class = "col-3 recommend_books">
                     <div style ="background-color: white;cursor: pointer; margin-right: 5px">
                         <img src="/Content/images/books/${item.AnhBia}" style="width:164px; height:235px; display:block;margin: auto" />
@@ -89,16 +46,109 @@ async function fetchData(loaiTaiLieu) {
                 </div>
             `).join("");
     document.getElementById("matching_books").innerHTML = danhSachHTML;
+
 }
-function truncate(str, maxLines) {
+const showNextPage = () => {
+    if (currentPage == lastPage)
+        increasePageList()
+    else {
+        currentPage++;
+        const lstItemPage = document.querySelectorAll(".pagination-homepage li");
+        lstItemPage.forEach(item => {
+            if (item.textContent == currentPage)
+                item.classList.add('active')
+            else
+                item.classList.remove('active')
+        });
+    }
+    changeNumberPage(currentPage, numberItemPerPage)
+}
+const showPreviousPage = () => {
+    if (currentPage == firstPage)
+        decreasePageList()
+    else {
+        currentPage--;
+        const lstItemPage = document.querySelectorAll(".pagination-homepage li");
+        lstItemPage.forEach(item => {
+            if (item.textContent == currentPage)
+                item.classList.add('active')
+            else
+                item.classList.remove('active')
+        });
+    }
+    changeNumberPage(currentPage, numberItemPerPage)
+}
+const decreasePageList = () => {
+    if (firstPage > 1) {
+        let listElementPage = document.querySelectorAll('.pagination-homepage li')
+        listElementPage.forEach(item => {
+            if (item.textContent != '«' && item.textContent != '»') {
+                item.textContent = Number(item.textContent) - 1
+            }
+
+        })
+        currentPage--;
+        firstPage--;
+        lastPage--;
+    }
+}
+const increasePageList = () => {
+    if (lastPage < totalPages) {
+        let listElementPage = document.querySelectorAll('.pagination-homepage li')
+        listElementPage.forEach(item => {
+            if (item.textContent != '«' && item.textContent != '»') {
+                item.textContent = Number(item.textContent) + 1
+            }
+
+        })
+        currentPage++;
+        lastPage++;
+        firstPage++;
+    }
+}
+const showNumberPages = async (loaiTaiLieu, currentPage) => {
+    const url = `/TaiLieu/GetDocumentByCategory?type=${loaiTaiLieu}`;
+    const response = await fetch(url, { method: 'GET' });
+    allDocuments = await response.json()
+
+    changeNumberPage(currentPage, numberItemPerPage)
+
+    totalPages = Math.ceil(allDocuments.length / numberItemPerPage)
+    const paginationElement = document.querySelector('.pagination-homepage')
+    paginationElement.innerHTML = `<li>&laquo;</li>`
+    paginationElement.innerHTML += `<li class = "active">1</li>`
+    if (totalPages > limitedDisplayPage) {
+        for (let i = 2; i <= limitedDisplayPage; i++) {
+            paginationElement.innerHTML += `<li>${i}</li>`
+        }
+        lastPage = limitedDisplayPage;
+    }
+    else {
+        for (let i = 2; i <= totalPages; i++) {
+            paginationElement.innerHTML += `<li>${i}</li>`
+        }
+        lastPage = totalPages;
+    }
+    firstPage = 1
+    paginationElement.innerHTML += `<li>&raquo;</li>`
+    addEffectClickPage()
+}
+const truncate = (str, maxLines) => {
     const words = str.split(' ');
     if (words.length > maxLines) {
         return words.slice(0, maxLines).join(' ') + '...';
     }
     return str;
 }
-function viewDetail(id) {
+const viewDetail = (id) => {
     const url = `/TaiLieu/Detail?id=${id}`;
     window.location.href = url;
 }
+
+let lastPage = 0, firstPage = 0, currentPage = 1, totalPages = 0;
+const numberItemPerPage = 12, limitedDisplayPage = 2;
+let allDocuments = []
+showNumberPages("book", 1);
+
+
 
